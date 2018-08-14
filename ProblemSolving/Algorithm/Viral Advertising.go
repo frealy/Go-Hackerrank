0edb573027d83ef513b92f006ceb98379f2d6938
M Fhadil <mfhadil05@gmail.com>
2018-08-14 10:45:58 +0700

    Viral Advertising

---
+++ ProblemSolving/Algorithm/Viral Advertising.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the viralAdvertising function below.
+func viralAdvertising(n int32) int32 {
+	var like int32 = 0
+	var val int32 = 5
+	for i := int32(0); i < n; i++ {
+		like = like + val/2
+		val = val / 2 * 3
+		fmt.Println(like, val)
+	}
+	return like
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	result := viralAdvertising(n)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
